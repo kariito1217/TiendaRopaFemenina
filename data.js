7c6a6d8b2073aa1c703b9f7165f217733c06d023
kariito1217 <karensuarez022@gmail.com>
2024-10-23 19:56:30 -0500

    e

---
+++ data.js
@@ -0,0 +1,39 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Camisa",
+        precio: 119.000,
+        categoria: "Camisas",
+        talla: ["S", "M", "L", "XL"],
+        color: ["Blanco"],
+        descripcion: "Camisa manga corta en tela tejida y largo regular, con molde amplio, botones contramarcados.",
+        material: "Algodón",
+        stock: 50,
+        imagen: "url-de-la-imagen"
+    },
+    {
+        id: 2,
+        nombre: "Jeans Skinny",
+        precio: 39.99,
+        categoria: "Pantalones",
+        talla: ["28", "30", "32", "34", "36"],
+        color: ["Azul Oscuro", "Negro"],
+        descripcion: "Jeans ajustados de mezclilla, cómodos y con estilo.",
+        material: "Mezclilla",
+        stock: 30,
+        imagen: "url-de-la-imagen"
+    },
+    {
+        id: 3,
+        nombre: "Chaqueta de Cuero Negra",
+        precio: 89.99,
+        categoria: "Chaquetas",
+        talla: ["S", "M", "L"],
+        color: ["Negro"],
+        descripcion: "Chaqueta de cuero sintético con cierre frontal y bolsillos.",
+        material: "Cuero Sintético",
+        stock: 10,
+        imagen: "url-de-la-imagen"
+    },
+    // ... añade más productos hasta llegar a 40
+];
